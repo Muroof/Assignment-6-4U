e78755b414148aa76418274e5272289c528a99d3
ranam3235 <ranam3235@HRH-IC0027320.wrdsb.ca>
2017-11-15 10:00:58 -0500

    Initial Push

---
+++ src/School.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ranam3235
+ */
+public class School {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // YEAH YEAH
+    }
+}
