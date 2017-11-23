3a2c73d58046fa096059c3c662c29717fa42da33
ranam3235 <ranam3235@HRH-IC0027319.wrdsb.ca>
2017-11-23 10:09:56 -0500

    Worked on ImageLoader and Location, 23/11/2017

--- src/ImageLoader.java
+++ src/ImageLoader.java
@@ -1,12 +1,34 @@
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.swing.JComponent;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author ranam3235
  */
-public class ImageLoader {
-    
+public class ImageLoader extends JComponent {
+
+
+
+//    @Override
+//    public void paintComponent(Graphics g) {
+//        // there is an image to draw
+//        if (image != null) {
+//            g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), null);
+//
+//        }
+//
+//    }
+//
+//    public void setImage(BufferedImage img) {
+//        // store the image
+//        this.image = img;
+//        // request to draw
+//        repaint();
+//    }
 }
