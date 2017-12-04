e2fdb9e0e3537c6a9e6cf488536c2c3859929370
ranam3235 <ranam3235@HRH-IC0027319.wrdsb.ca>
2017-12-04 10:14:10 -0500

    Attempting to finish LocationLoader class

--- src/LocationLoader.java
+++ src/LocationLoader.java
@@ -14,12 +14,13 @@ import java.util.Scanner;
  */
 public class LocationLoader {
 
-    private BufferedImage image;
-    private BufferedImage[] images;
-    private String[] locationName;
-    private String[] direction;
-    private String[] imageName;
-    private boolean[] canMoveForward;
+//    private BufferedImage image;
+//    private BufferedImage[] images;
+//    private String[] locationName;
+//    private String[] direction;
+//    private String[] imageName;
+//    private boolean[] canMoveForward;
+    private LocationObject[] locations;
 
     /**
      * @param args the command line arguments
@@ -50,28 +51,55 @@ public class LocationLoader {
         in.nextLine();
         String initialDirection = in.nextLine();
 
-        int[] array = new int[122];
-
-
-
-
-
-        // make another array for space, double the size of the previous
-        int[] temp = new int[array.length * 2];
-        // copy values from original array into new array
-        for (int i = 0; i < array.length; i++) {
-            // fill the temporary array with all the values of the previous array
-            temp[i] = array[i];
+        locations = new LocationObject[122];
+        while (in.hasNext()) {
+            String name = in.nextLine();
+            String direction = in.next();
+            String imageName = in.next();
+            String isFrontBlocked = in.next();
+            
+            if (isFrontBlocked.equals("true")) {
+                boolean frontBlocked = true;
+            }
+            else if (isFrontBlocked.equals("false")) {
+                boolean frontBlocked = false;
+                String nextLocation = in.next();
+                String nextLocationDirection = in.next();
+                 
+            }
+            // create the contact
+            LocationObject l = new LocationObject(name);
+            l.setDirection(direction);
+            l.setImage(imageName);
+            l.setIsFrontBlocked(frontBlocked);
+//            // add contact to list
+//            locations[i] = l;
+//            i++;
         }
-        // set the tremporary array to be the new array
-        array = temp;
-        // insert the number at the end of the list
-        //array[numItems] = num;
-        // increase the number of items
-        // numItems++;
 
-        for (int i = 0; i < array.length; i++) {
-        }
+
+
+
+
+
+
+
+//        // make another array for space, double the size of the previous
+//        int[] temp = new int[array.length * 2];
+//        // copy values from original array into new array
+//        for (int i = 0; i < array.length; i++) {
+//            // fill the temporary array with all the values of the previous array
+//            temp[i] = array[i];
+//        }
+//        // set the tremporary array to be the new array
+//        array = temp;
+//        // insert the number at the end of the list
+//        //array[numItems] = num;
+//        // increase the number of items
+//        // numItems++;
+//
+//        for (int i = 0; i < array.length; i++) {
+//        }
 
 
     }
